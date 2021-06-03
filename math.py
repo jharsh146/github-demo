5a05695980d19232321eeb8960fa3946e28a58ba
Harsh Choudhary <jharsh146@gmail.com>
2021-06-03 14:46:19 +0530

    Added fns headers

--- math.py
+++ math.py
@@ -1,8 +1,15 @@
+#Add Implementation
 def add(x,y):
     pass
+    
+#Subtract implementation
 def subtract(x,y):
     pass
+    
+#Multiply implementation 
 def multiply(x,y):
     pass
+    
+#Division implementation
 def divide(x,y):
     pass
